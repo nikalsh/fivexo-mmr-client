43f2d0fc342bee436d5c2e58c9f2441ee1081237
Niklas <niklas@helander.se>
2020-04-13 03:40:33 +0200

    add game grid

--- src/Game.svelte
+++ src/Game.svelte
@@ -2,19 +2,49 @@
   let rows = 15;
   let cols = 15;
   let grid;
+  let game = true
 
-  // initGrid();
+  initGrid();
 
-  // function initGrid() {
-  //   grid = [...Array(rows)].map(x => Array(cols).fill(0));
-  // }
+  function initGrid() {
+    grid = [...Array(rows)].map(x => Array(cols).fill(" "));
+  }
 
-
-
+  function handleClick(i, j) {
+    grid[i][j] = "X"
+    console.log(i + " " + j);
+    
+  }
 </script>
 
 <style>
+  main {
+    margin: 0;
+  }
+  button {
+    margin: 0;
+    text-align: center;
+    width: 40px;
+    height: 40px;
+    font-weight: 600;
+    vertical-align: top;
+  }
 
+  .enabled {
+    cursor: pointer;
+  }
 </style>
 
-<main />
+<main>
+
+  {#each grid as row, i}
+    {#each row as col, j}
+      <button
+      class:enabled={game}
+      disabled={!game} 
+      on:click={() => handleClick(i, j)}>{grid[i][j]}</button>
+    {/each}
+    <br />
+  {/each}
+
+</main>
